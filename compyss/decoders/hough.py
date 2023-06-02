f49cc6fbb9067fde1e61629ceb49931e668964e9
benjamin potter <bp@benjamins-MacBook-Pro.local>
2023-06-02 13:00:12 -0400

    add first pass of hough transform method as a decoder

--- compyss/decoders/hough.py
+++ compyss/decoders/hough.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import math
+
 from compyss.sources.root import Image
 
 """
@@ -11,16 +12,38 @@ See the paper linked above for more information.
 
 def extract_binary(aolp_image: Image, threshold=0.1) -> np.array:
     """
-    Extract a binary image by sampling each pixel in an AoLP image. If |pixel - 90*| < threshold,
+    Extract a binary image by sampling each pixel in an AoLP image. If pixel is within threshold of 90*,
     mark the pixel as a one in the binary image.
     """
 
     bin_image = np.empty_like(aolp_image.aolp)
     for y in range(len(aolp_image.aolp)):
         for x in range(len(aolp_image.aolp[y])):
-            bin_image[y][x] = 255 if math.fabs(aolp_image.aolp[y][x] - 90) < threshold else 0
+            bin_image[y][x] = 0
+            if aolp_image.aolp[y][x] < (-90 + threshold) or aolp_image.aolp[y][x] > (90 - threshold):
+                bin_image[y][x] = 255
 
     return bin_image
+
+
+def draw_line_overlay(image, line):
+    """
+    Draw a line on an image and write it to resources directory.
+
+    TODO Move this to a utility module.
+    """
+
+    rho = line[0][0]
+    theta = line[0][1] 
+    a = math.cos(theta)
+    b = math.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+    pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+    cv.line(image, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+
+    cv.imwrite("res/line_overlay.png", image)
     
 
 def hough_transform(bin_image: np.array) -> float:
@@ -28,7 +51,24 @@ def hough_transform(bin_image: np.array) -> float:
     Perform Hough transform on a binary image.
     Return an angle.
     """
-    
 
-    return 0.0
+    line_threshold = 300
+    lines = cv.HoughLines(bin_image.astype('uint8'), 1, np.pi / 180, line_threshold, None, 0, 0)
+
+    if lines is None:
+        return 0.0
+    
+    """
+    https://stackoverflow.com/questions/36021556/get-rho-and-theta-with-miximum-votes-from-cv2-houghlines
+
+    According to above post, the lines list is sorted by votes. I will pick the first line to use as an angle
+    since it has the highest number of votes in accumulator space.
+    """
+
+    s_meridian = lines[0]
+
+    # line_display = cv.cvtColor(np.float32(bin_image), cv.COLOR_GRAY2BGR)
+    # draw_line_overlay(line_display, s_meridian)
+        
+    return s_meridian[0][1]
      
--- examples/solar_meridian.py
+++ examples/solar_meridian.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import compyss.core
 from compyss.sources.file import FileSource
 
@@ -7,10 +9,10 @@ Read angle between camera and solar meridian.
 
 def main():
     
-    cmps = compyss.core.Compass(source=FileSource("res/aolp_test.png"))
+    cmps = compyss.core.Compass(source=FileSource("res/aolp_test3.png"))
     angle_to_sm = cmps.read_to_sm()
 
-    print(angle_to_sm)
+    print(angle_to_sm * 180/np.pi)
 
 if __name__ == "__main__":
     main()
