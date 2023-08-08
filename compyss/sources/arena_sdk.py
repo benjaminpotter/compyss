140a54b437f175895a2c3a40d62991c7955f9720
ben potter <baffqd@gmail.com>
2023-08-08 15:50:45 -0400

    fix arena sdk source
    - use Mono8 output and process resulting image using polanalyser
    - use matplotlib to generate aolp visual

--- compyss/sources/arena_sdk.py
+++ compyss/sources/arena_sdk.py
@@ -4,36 +4,43 @@ from arena_api.system import system
 from arena_api.buffer import BufferFactory
 
 import numpy as np
+import polanalyser as pa
 
 class ArenaSDK(ImageSource):
 
     """
         Source for LUCID Vision Cameras.
         
-        Configuration options map to camera configuration nodes.
-        https://support.thinklucid.com/phoenix-phx050-pq-polarized/#2931
+        Configuration options are loaded from a "streamable" file. This file can be generated using ArenaView GUI software.
+        More information: https://support.thinklucid.com/documentation/configuring-the-camera-and-host-system/user-sets-streamables-and-file-access/streamables/
+        TODO: Camera supports saving common streamable files to on-board storage.
         
-        Support for streamable file as well. Configuration options will override streamables.
+        Loads a light intensity image from the camera using the Mono8 pixel format. This image contains light intensity for 0, 90, 45, 135 angles.
+        vector is then constructed for each pixel. From the Stokes vector, AoLP and DoLP are computed.
+        More information: https://support.thinklucid.com/knowledgebase/pixel-formats/#mono8
+        TODO: Eliminate dependency on Polanalyser.
+        
+        TODO: Fix exposure settings to improve execution time.
     
     """
 
-    def __init__(self): ## REMOVE config options
+    def __init__(self, streamable_file):
         
         self.device = None
-        self.config = {'PixelFormat':'PolarizedStokes_S0_S1_S2_S3_Mono8'}
-        self.streamable_file = "res/features.txt"
+        self.pixel_format = 'Mono8'
+        self.streamable_file = streamable_file
         self.buffer_count = 25
         
         self.device = self._connect_to_device() # on fail, raise exception
         if self.device is None:
             raise Exception("Failed to connect to camera.")
             
-        self._configure_active_device(self.device, self.config, self.streamable_file)
-        self._start_stream(self.device, self.buffer_count)
+        self._configure_active_device()
+        self._start_stream()
      
     def __del__(self):
         if self.device is not None:
-            self._stop_stream(self.device)
+            self._stop_stream()
             
         system.destroy_device()
         
@@ -46,27 +53,21 @@ class ArenaSDK(ImageSource):
         
         return device_list[0]
         
-    def _configure_active_device(self, device, config, streamable_file):
+    def _configure_active_device(self):
         # apply streamable file
-        device.nodemap.read_streamable_node_values_from(streamable_file)
+        self.device.nodemap.read_streamable_node_values_from(self.streamable_file)
         
-        device.tl_stream_nodemap["StreamBufferHandlingMode"].value = "NewestOnly"
-        device.tl_stream_nodemap['StreamAutoNegotiatePacketSize'].value = True
-        device.tl_stream_nodemap['StreamPacketResendEnable'].value = True
+        self.device.tl_stream_nodemap["StreamBufferHandlingMode"].value = "NewestOnly"
+        self.device.tl_stream_nodemap['StreamAutoNegotiatePacketSize'].value = True
+        self.device.tl_stream_nodemap['StreamPacketResendEnable'].value = True
     
-        # grab required nodes
-        nodes = device.nodemap.get_node(list(config))
+        self.device.nodemap["PixelFormat"].value = self.pixel_format
         
-        # set nodes
-        for opt in config:
-            nodes[opt].value = config[opt]
-            print(f"Set {opt} to {config[opt]}")
+    def _start_stream(self):
+        self.device.start_stream(self.buffer_count)
         
-    def _start_stream(self, device, buffer_count):
-        device.start_stream(buffer_count)
-        
-    def _stop_stream(self, device):
-        device.stop_stream()
+    def _stop_stream(self):
+        self.device.stop_stream()
         
     def get(self):
         
@@ -84,23 +85,23 @@ class ArenaSDK(ImageSource):
             f'Bpp = {buffer_copy.bits_per_pixel}')
         
         image = Image()
+        image.pixels = np.ctypeslib.as_array(buffer_copy.pdata, (buffer_copy.height, buffer_copy.width))
         
-        # PolarizedStokes_S0_S1_S2_S3_Mono8 -> 32 bits, 4 bytes per pixel
-        # Reference: https://support.thinklucid.com/knowledgebase/pixel-formats
+        # ensure each pixel is atleast 0000 0001 to prevent divide by zero later
+        np.clip(image.pixels, 1, 255, image.pixels) 
         
-        image.pixels = np.ctypeslib.as_array(buffer_copy.pdata, (buffer_copy.height, buffer_copy.width, buffer_copy.bits_per_pixel // 8))
+        # demosaic
+        intensity_imgs = pa.demosaicing(image.pixels, pa.COLOR_PolarMono)
         
-        #image.stokes = np.divide(image.pixels, np.stack((image.pixels[:,:,0],image.pixels[:,:,0],image.pixels[:,:,0],image.pixels[:,:,0]), axis=-1))
-        #image.stokes = np.copy(image.pixels) # normalize
-        image.stokes = np.clip(image.pixels, 1, 255)
-       
-        s0 = image.stokes[:,:,0]
-        s1 = image.stokes[:,:,1]
-        s2 = image.stokes[:,:,2]
+        # generate stokes
+        angles = np.deg2rad([0, 45, 90, 135])
+        image.stokes = pa.calcStokes(intensity_imgs, angles)
         
-        # Reference: https://en.wikipedia.org/wiki/Stokes_parameters
-        image.aolp = np.mod(0.5 * np.arctan2(s2, s1), np.pi)
-        image.dolp = np.sqrt(s1**2 + s2**2) / s0 # S_3 == 0 for all pixels.
+        # generate dolp 
+        image.dolp = pa.cvtStokesToDoLP(image.stokes)  # [0, 1]
+        
+        # convert to aolp
+        image.aolp = 0.5 * np.arctan2(image.stokes[..., 2], image.stokes[..., 1]) # [-pi/2, pi/2]
    
         return image
         
--- examples/load_from_arena.py
+++ examples/load_from_arena.py
@@ -1,34 +1,23 @@
-import numpy as np
-import cv2
+import matplotlib.pyplot as plt
 
 import compyss.core
-from compyss.sources.arena_sdk import ArenaSDK
+from compyss.sources.arena import ArenaSDK
 
 """
 ArenaSDK example implementation.
 
-This example uses the ArenaSDK as an ImageSource and converts it to HSV displayable format.
-It is currently under construction pending the completion of the ArenaSDK source.
+This example uses the ArenaSDK as an ImageSource and builds a figure using matplotlib.
 
 """
 
-import matplotlib.pyplot as plt
-
 def main():
     
-    cmps = compyss.core.Compass(source=ArenaSDK())
+    cmps = compyss.core.Compass(source=ArenaSDK(streamable_file="res/features.txt"))
     image = cmps.source.get()
     
-    hsv = np.empty((2048, 2048, 3), dtype=np.uint8)
-    hsv[:,:,0] = image.aolp * 180.0 / np.pi
-    hsv[:,:,1] = 255 # image.dolp * 255
-    hsv[:,:,2] = 255
+    pos = plt.imshow(image.aolp, cmap="jet")
+    plt.colorbar(pos)
+    plt.show()
     
-    cv2.imwrite("PolarizedStokes_S0_Mono8.png", image.stokes[:, :, 0])
-    cv2.imwrite("PolarizedStokes_S1_Mono8.png", image.stokes[:, :, 1])
-    cv2.imwrite("PolarizedStokes_S2_Mono8.png", image.stokes[:, :, 2])
-    cv2.imwrite("PolarizedStokes_S3_Mono8.png", image.stokes[:, :, 3])
-    cv2.imwrite("AoLP_HSV.png", cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
-
 if __name__ == "__main__":
     main()
